7ef42eee99e3ccb5af7d79a2a03c4bc829e793e0
Kousthubh Yadavalli <kyadavalli04@gmail.com>
2023-07-05 14:31:42 +0530

    addNote feature added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NoteState from './context/notes/NoteState';
+import Alert from './components/Alert';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <NoteState>
+      <Router>
+    <Navbar/>
+    <Alert message="This is awesome"/>
+     <Routes>
+      <Route exact path="/" element={<Home/>}></Route>
+      <Route exact path="/about" element={<About/>}></Route>
+     </Routes>
+      </Router>
+
+      
+      </NoteState>
+
+      </>
   );
 }
 
